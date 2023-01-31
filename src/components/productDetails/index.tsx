ee0bdf9d1a44365f0a0e96222690f1ccfbc5a25d
Hafis muhammed <42732514+hafis11@users.noreply.github.com>
2023-01-31 16:39:52 +0530

    mobile responsive

--- src/components/productDetails/index.tsx
+++ src/components/productDetails/index.tsx
@@ -4,7 +4,7 @@ import Gallery from "./component/gallery";
 
 function ProductDetails() {
   return (
-    <div className="xl:container h-screen w-screen px-3 xl:px-0 lg:mx-auto grid grid-rows-2 md:grid-rows-1 md:grid-cols-2 overflow-auto py-2 select-none scrollbar-hide">
+    <div className="xl:container h-screen w-screen md:px-3 xl:px-0 lg:mx-auto grid grid-rows-1 md:grid-rows-1 md:grid-cols-2 overflow-auto md:py-2 select-none scrollbar-hide">
       <Details />
       <Gallery />
     </div>
